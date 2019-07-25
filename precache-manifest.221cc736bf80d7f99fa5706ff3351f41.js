fa1a7f36e4b14e1c21cb9ad4ce96f0078f1b8ee0
jfcarocota <jfcarocota@gmail.com>
2019-07-24 21:27:03 -0700

    Updates

---
+++ precache-manifest.221cc736bf80d7f99fa5706ff3351f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1f756860bd0283b4098c5882c46818",
+    "url": "/dsc-udavinci/index.html"
+  },
+  {
+    "revision": "74a609e4d66161caf3af",
+    "url": "/dsc-udavinci/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dsc-udavinci/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "74a609e4d66161caf3af",
+    "url": "/dsc-udavinci/static/js/main.749f1746.chunk.js"
+  },
+  {
+    "revision": "9c6f9fa9a8ca5d576ac3",
+    "url": "/dsc-udavinci/static/js/runtime~main.6c06b24c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dsc-udavinci/static/media/logo.5d5d9eef.svg"
+  }
+]);
